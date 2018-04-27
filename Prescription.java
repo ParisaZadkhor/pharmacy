d738f7700a2026c6bfb9afdcb4c4f749f46d2267
ZahraShayesteh <37510195+ZahraShayesteh@users.noreply.github.com>
2018-04-28 01:20:28 +0430

    updating prescription

--- Prescription.java
+++ Prescription.java
@@ -4,10 +4,10 @@ import java.util.ArrayList;
 import java.util.Date;
 
 public class Prescription {
-	private int ID;
-	private Date visitDate;
-	private double price;
-	private ArrayList<Medicine> medicine;
+	private int ID;//prescription ID
+	private String visitDate;
+	private int m_p;//medicine prescription ID
+	private int patient_ID;//PAtient ID that a prescription related to
 
 	public int getID() {
 		return ID;
@@ -17,36 +17,42 @@ public class Prescription {
 		ID = iD;
 	}
 
-	public Date getVisitDate() {
+	public String getVisitDate() {
 		return visitDate;
 	}
 
-	public void setVisitDate(Date visitDate) {
+	public void setVisitDate(String visitDate) {
 		this.visitDate = visitDate;
 	}
 
-	public double getPrice() {
-		return price;
+	public int getM_p() {
+		return m_p;
 	}
 
-	public void setPrice(double price) {
-		this.price = price;
+	public void setM_p(int m_p) {
+		this.m_p = m_p;
 	}
 
-	public ArrayList<Medicine> getMedicine() {
-		return medicine;
+	public int getPatient_ID() {
+		return patient_ID;
 	}
 
-	public void setMedicine(ArrayList<Medicine> medicine) {
-		this.medicine = medicine;
+	public void setPatient_ID(int patient_ID) {
+		this.patient_ID = patient_ID;
 	}
 
-	public Prescription(int ID, Date visitDate, double price, ArrayList<Medicine> medicine) {
+	public Prescription(int iD, String visitDate, int mp, int patient_ID) {
+		ID = iD;
+		this.visitDate = visitDate;
+		setM_p(mp);
+		this.patient_ID = patient_ID;
+	}
+
+	public Prescription(int ID, String visitDate, int mp) {
 		this.ID = ID;
 		this.visitDate = visitDate;
-		this.price = price;
-		this.medicine = new ArrayList<Medicine>();
-		setMedicine(medicine);
+		setM_p(mp);
+		setPatient_ID(-1);
 	}
 
 	public Prescription() {
